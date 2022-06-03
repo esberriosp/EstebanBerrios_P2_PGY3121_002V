d3c327dee03783545007ad971b4c11ba863e22f7
esberriosp <es.berriosp@duocuc.cl>
2022-06-03 09:45:08 -0400

    commi 3

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-
 function getBasePath(service) {
-    return "https://chilealerta.com/api/query/?user=demo&select=" + service;
+    return "https://chilealerta.com/api/query/?user=andr3iss&select=" + service;
 }
 
 function getBasePathindex(service) {
     return "https://chilealerta.com/api/query/?user=andr3iss&select=" + service +"&limit={1}";
 }
-
 function createRowDisaster(reference, magnitude, latitude, longitude, local_time) {
     return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + local_time + '</td></tr>';
 }
 
-function createRowAlert(post_title, magnitude, latitude, longitude, alert_date) {
-    return '<tr><td>' + post_title + '</td><td>' + ' ' + '</td><td>' + ' ' + '</td><td>' + ' ' + '</td><td>' + alert_date + '</td></tr>';
+function createRowOnemiDisaster(state, post_title, alert_region, alert_date, url) {
+    return '<tr><td>' + state + '</td><td>' + post_title + '</td><td>' + alert_region + '</td><td>' + alert_date + '</td><td><a href="' + url + '">' + url + '</a></td></tr>';
 }
 
+function createRowMundoDisaster(reference, magnitude, latitude, longitude, utc_time) {
+    return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + utc_time + '</td></tr>';
+}
 function enviarformulariovalidado() {
     var nombre = document.getElementById('nombreUsuario');
     var correo = document.getElementById('correoElectronico');
